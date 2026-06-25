2808ab01e324724c816f4a2b540c536d7a67978c
petereog <ogungbepeter1@gmail.com>
2026-06-25 08:00:58 +0100

    completed the grocery screen

--- lib/grocery/view/pages/grocery_screen.dart
+++ lib/grocery/view/pages/grocery_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({super.key});
 
@@ -10,22 +9,306 @@ class GroceryScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
-                'Grocery Delivery at Your Doorstep',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Your favorite groceries delivered fast at your door.',
-                style: TextStyle(fontSize: 16),
-              ),
-            ]
-          )
-          )
-      )
-          );
-          }
-          }
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Search bar
+                Container(
+                  height: 52,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEEEEEE),
+                    borderRadius: BorderRadius.circular(1),
+                  ),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 16),
+                      const Icon(Icons.search, color: Color.fromARGB(221, 0, 0, 0), size: 22),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          'Search stores and product..',
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 24),
+
+                // Address row
+                Row(
+                 
+                  children: [
+                    Icon(Icons.location_on, size: 28,),
+                    const SizedBox(width: 45,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('San Fransico Bay area', style: TextStyle( fontSize: 18, fontWeight:.bold, ),),
+                        Text('Johns list', style: TextStyle(fontWeight:.w500),),
+                      ],
+                    ),
+                    const SizedBox(width: 95,),
+                    Icon(Icons.arrow_forward_ios_sharp),
+                  ],
+                ),
+
+                const SizedBox(height: 24),
+
+                // Store cards row 1
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 168,
+                      width: 185,
+                      color: const Color(0xFFF60000),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/Safeway logo.png'),
+                          const SizedBox(height: 40),
+                          Container(
+                            height: 25,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'in 60 minutes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 168,
+                      width: 185,
+                      color: const Color(0xFF77A240),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/Adronic.png'),
+                          const SizedBox(height: 40),
+                          Container(
+                            height: 25,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'in 60 minutes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 24),
+
+                // Store cards row 2
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 168,
+                      width: 185,
+                      color: const Color(0xFFFE4AE3C),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/5element logo.png'),
+                          const SizedBox(height: 40),
+                          Container(
+                            height: 25,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'in 60 minutes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 168,
+                      width: 185,
+                      color: const Color(0xFFB71234),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/layer1.png'),
+                          const SizedBox(height: 40),
+                          Container(
+                            height: 25,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'in 60 minutes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 24),
+
+                // Store cards row 3
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 168,
+                      width: 185,
+                      color: const Color(0xFFAB0920),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/Cardenas logo.png'),
+                          const SizedBox(height: 40),
+                          Container(
+                            height: 25,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'in 60 minutes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 168,
+                      width: 185,
+                      color: const Color(0xFFD2202F),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/smart&final logo.png'),
+                          const SizedBox(height: 40),
+                          Container(
+                            height: 25,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'in 60 minutes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 24),
+
+                // Store cards row 4
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 168,
+                      width: 185,
+                      color: const Color(0xFFF60000),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/Safeway logo.png'),
+                          const SizedBox(height: 40),
+                          Container(
+                            height: 25,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'in 60 minutes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 168,
+                      width: 185,
+                      color: const Color(0xFF77A240),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/Adronic.png'),
+                          const SizedBox(height: 40),
+                          Container(
+                            height: 25,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'in 60 minutes',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 24),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
